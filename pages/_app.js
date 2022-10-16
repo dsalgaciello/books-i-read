6b375b03b120c7e905f2c0cf5ff939fc41126a21
Darwin Salgaciello <dsalgaciello@cadence-labs.com>
2022-10-16 07:50:35 -0400

    Add images and bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>The Books I Read!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -16,8 +17,28 @@ export default function Home() {
       <main>
         <Header title="The Books I Read" />
         <ul class={"customers"}>
-           <li class={"customer"}> <a href="cesco">Francesco</a></li>
-           <li class={"customer"}> <a href={"cenzo"}>Vincenzo</a></li>
+           <li class={"customer"}>
+               <div class={"row"}>
+                   <div class={"col-8"}>
+                       <a href={"cesco"}><img alt={"cesco"} src={"cesco.jpg"}/></a>
+                   </div>
+                   <div class={"col-4"}>
+                       <a href="cesco">Francesco</a><br/>
+                       6 years old
+                   </div>
+               </div>
+           </li>
+           <li class={"customer"}>
+               <div class={"row"}>
+                   <div className={"col-8"}>
+                      <a href={"cenzo"}><img alt={"cenzo"} src={"cenzo.jpg"} /></a>
+                   </div>
+                   <div className={"col-4"}>
+                       <a href={"cenzo"}>Vincenzo</a><br/>
+                       3 years old
+                   </div>
+               </div>
+           </li>
         </ul>
       </main>
 
