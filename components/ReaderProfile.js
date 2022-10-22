56d2abbbc94760208be4f1fe1bc4418dcb18e119
Darwin Salgaciello <dsalgaciello@cadence-labs.com>
2022-10-22 03:07:35 -0400

    Modify reader profile

--- components/ReaderProfile.js
+++ components/ReaderProfile.js
@@ -1,10 +1,10 @@
 export default function ReaderProfile({reader}) {
   return (
       <div className={"row customer"}>
-          <div className={"col-8 col-sm-8 customer-image"}>
+          <div className={"col-5 col-sm-5 customer-image"}>
               <a href={reader.link}><img alt={reader.name} src={reader.image}/></a>
           </div>
-          <div className={"col-4 col-sm-4"}>
+          <div className={"col-7 col-sm-7"}>
               <a href={reader.link}>{reader.name}</a><br/>
               {reader.age}
           </div>
